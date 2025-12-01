d33bcaa5b9a890bba7637102058a0e7e193ac0d7
faloger <gtllop098@gmail.com>
2025-12-01 15:55:39 +0300

    Create main.py

---
+++ task_1/main.py
@@ -0,0 +1,39 @@
+import requests  # Импортируем библиотеку requests для выполнения HTTP-запросов
+from bs4 import BeautifulSoup  # Импортируем BeautifulSoup для парсинга HTML-страниц
+import json  # Импортируем json для сохранения данных в файл
+from urllib.parse import urljoin  # Импортируем urljoin для правильного объединения URL-адресов
+
+def get_quotes_from_page(url):  # Объявляем функцию для сбора цитат с страницы, принимает URL
+    all_quotes = []  # Создаем список для хранения всех цитат
+    quote_number = 1  # Инициализируем счетчик цитат для нумерации
+
+    while url:  # Пока есть URL (пока есть страница для обработки)
+        headers = {  # Определяем заголовки для HTTP-запроса
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/58.0.3029.110 Safari/537.3"  # Имитация браузера
+        }
+        response = requests.get(url, headers=headers)  # Отправляем GET-запрос по текущему URL с заголовками
+        if response.status_code != 200:  # Если ответ не 200 OK
+            print("Ошибка загрузки страницы")  # Выводим сообщение об ошибке
+            break  # Выходим из цикла, прерывая обработку
+        soup = BeautifulSoup(response.text, 'html.parser')  # Создаем объект BeautifulSoup для парсинга HTML
+        quote_blocks = soup.find_all("div", class_="quote")  # Находим все блоки цитат на странице
+
+        for block in quote_blocks:  # Проходим по каждому блоку цитаты
+            text = block.find("span", class_="text").get_text(strip=True)  # Извлекаем текст цитаты, очищая лишние пробелы
+            all_quotes.append({"number": quote_number, "quote": text})  # Добавляем цитату с ее номером в список
+            print (f'{quote_number}. {text}') # Выводм цитаты на экран
+            quote_number += 1  # Увеличиваем счетчик цитат
+
+        next_btn = soup.find('li', class_='next')  # Ищем кнопку "следующая" страница
+        if next_btn and next_btn.a:  # Если кнопка есть и есть ссылка внутри
+            url = urljoin(url, next_btn.a['href'])  # Обновляем URL на следующую страницу
+        else:  # Если кнопки или ссылки нет, значит это последняя страница
+            url = None  # Устанавливаем url в None, чтобы завершить цикл
+
+    return all_quotes  # Возвращаем список собранных цитат
+
+if __name__ == '__main__':  # Проверка, что скрипт запущен напрямую, а не импортирован
+    start_url = 'https://quotes.toscrape.com/'  # Начальный URL для парсинга
+    quotes = get_quotes_from_page(start_url)  # Вызываем функцию сбора цитат
